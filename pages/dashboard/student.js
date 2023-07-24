11f076f15102604b9f71c40a952eea5a65461b03
Emmanuel Oguntola <oguntolagifted@gmail.com>
2023-07-24 03:07:55 +0100

    Add files via upload

--- pages/dashboard/student.js
+++ pages/dashboard/student.js
@@ -7,13 +7,21 @@ function student() {
   const user = useSelector(selectUser);
   console.log(user);
 
+  const username = user.name ? user.name : "";
+  const matric = user.matric ? user.matric : "";
+  const tutorial = user.tutorial ? user.tutorial : "";
+  const practical = user.practical ? user.practical : "";
+  const test = user.test ? user.test : "";
+  const exam = user.exam ? user.exam : "";
+  const total = user.total ? user.total : "";
+
   return (
     <div className={styles.main}>
       <div className={styles.top}>
         <div className={styles.left_top}>
           <h2>Check Results</h2>
-          <p>Name: {user.name}</p>
-          <p>Matric number: {user.matric}</p>
+          <p>Name: {username}</p>
+          <p>Matric number: {matric}</p>
           <p>Faculty: Technology</p>
           <p>Department: Computer Science and Engineering</p>
           <p>Level: 2</p>
@@ -27,7 +35,7 @@ function student() {
             <tr>
               <th>Course Code</th>
               <th className={styles.title}>Course Title</th>
-              <th>Unit</th>
+              <th>Unit </th>
               <th>Tutorial</th>
               <th>Practical</th>
               <th>Test</th>
@@ -41,11 +49,11 @@ function student() {
               <td>CSC201</td>
               <td>Computer Programming 1</td>
               <td>2</td>
-              <td>{user.tutorial}</td>
-              <td>{user.practical}</td>
-              <td>{user.test}</td>
-              <td>{user.exam}</td>
-              <td>{user.total}</td>
+              <td>{tutorial}</td>
+              <td>{practical}</td>
+              <td>{test}</td>
+              <td>{exam}</td>
+              <td>{total}</td>
               <td style={{ fontSize: "20px", fontWeight: "500" }}>
                 {user.feedback}
               </td>
